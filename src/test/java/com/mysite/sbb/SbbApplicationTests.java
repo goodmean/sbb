a3d583b2128ee0a17d95b214649091692062ebde
goodmean <zizigoo12@gmail.com>
2022-04-23 16:32:41 +0900

    2-05 리포지터리, 데이터 조회하기, findBySubjectLike

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -17,6 +17,12 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	void testFindBySubjectLike() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
 	@Test
 	void testFindBySubjectAndContent() {
 		Question q = this.questionRepository.findBySubjectAndContent(
